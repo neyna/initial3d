e33e5097741f97704ba4ad573b41a6e3baba9a73
Neyna <commiter@fake.com>
2013-01-11 17:59:51 +0100

    first working cmake version

--- src/test.c
+++ src/test.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "test.h"
+#include <test.h>
 
 int main (int argc, char *argv[]) {
-  print("Hello world !\n");
+  printf("Hello world !\n");
   fprintf(stdout,"Software version : %d.%d\n",
             Test_VERSION_MAJOR,
             Test_VERSION_MINOR);
