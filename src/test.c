fe1d5cc9af3983e59fff69a750cef5bced119b66
Neyna <commiter@fake.com>
2013-01-16 11:03:12 +0100

    changed variables to remove the need to specify the directory name, making the build process less name refactoring sensible

--- src/test.c
+++ src/test.c
@@ -5,7 +5,7 @@
 int main (int argc, char *argv[]) {
   printf("Hello world !\n");
   fprintf(stdout,"Software version : %d.%d\n",
-            Test_VERSION_MAJOR,
-            Test_VERSION_MINOR);
+            TEST_VERSION_MAJOR,
+            TEST_VERSION_MINOR);
   return 0;
 }
